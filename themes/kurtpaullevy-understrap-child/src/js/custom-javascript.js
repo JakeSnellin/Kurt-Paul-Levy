c310fc952e4e2a553feb6c2cda86d5b621af61c5
Jake Snellin <jake_snellin@hotmail.co.uk>
2025-03-20 13:13:59 +0000

    Implement scroll progress bar.

--- themes/kurtpaullevy-understrap-child/src/js/custom-javascript.js
+++ themes/kurtpaullevy-understrap-child/src/js/custom-javascript.js
@@ -1,11 +1,13 @@
 // Add your custom JS here.
 
 import { header } from "./header-navigation.js";
+import { progressBar } from "./progress-bar.js";
 import { validateAndRegisterUser } from "./validation/userValidation.js";
 
 (function($) {
 
     $(document).ready(function() {
+        progressBar($);
         header.publicToggleShowNav();
         validateAndRegisterUser($);  // Call the function directly
     });

3cd97362a534874e1a1d5839793685fa2bee1150
Jake Snellin <jake_snellin@hotmail.co.uk>
2025-03-23 10:09:50 +0000

    Add smooth fade transitions for page navigation.

--- themes/kurtpaullevy-understrap-child/src/js/custom-javascript.js
+++ themes/kurtpaullevy-understrap-child/src/js/custom-javascript.js
@@ -3,13 +3,15 @@
 import { header } from "./header-navigation.js";
 import { progressBar } from "./progress-bar.js";
 import { validateAndRegisterUser } from "./validation/userValidation.js";
+import { pageTransition } from "./page-transition.js";
 
 (function($) {
 
     $(document).ready(function() {
+        pageTransition($);
         progressBar($);
         header.publicToggleShowNav();
-        validateAndRegisterUser($);  // Call the function directly
+        validateAndRegisterUser($); 
     });
 
 })(jQuery);

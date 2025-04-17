1a96bc0d3d115193b6ee87e10e6c83a41909e3fc
Jake Snellin <jake_snellin@hotmail.co.uk>
2025-04-17 13:27:47 +0100

    Implement close menu on outside click.

--- themes/kurtpaullevy-understrap-child/js/child-theme.js
+++ themes/kurtpaullevy-understrap-child/js/child-theme.js
@@ -7072,81 +7072,62 @@
 	  });
 	}
 
-	//refactor this function using event delegation. 
 	function categoryDropdown($) {
-	  const dropdownMenuSidebarVariant = $('.widget-area .category-dropdown-menu');
-	  const dropdownButtonSidebarVariant = $('.widget-area #dropdownMenuButton');
-	  const dropdownMenuHeaderVariant = $('.site-main .category-dropdown-menu');
-	  const dropdownButtonHeaderVariant = $('.site-main #dropdownMenuButton');
-	  const dropdownSidebarVariant = $('.widget-area .category-dropdown');
-	  const dropdownHeaderVariant = $('.site-main .category-dropdown');
-	  const caretIcon = $('.caret-icon');
-	  dropdownButtonSidebarVariant.on('click', function () {
+	  function toggleDropdown({
+	    button,
+	    menu,
+	    container
+	  }) {
+	    const caretIcon = container.find('.caret-icon');
 	    caretIcon.toggleClass('rotated');
-	    dropdownMenuSidebarVariant.toggleClass('show-menu');
-	    if (dropdownMenuSidebarVariant.hasClass('show-menu')) {
-	      dropdownSidebarVariant.css('background-color', "#EFEFEF");
-	      dropdownMenuSidebarVariant.slideDown(300);
-	      dropdownButtonSidebarVariant.attr('aria-expanded', 'true');
+	    menu.toggleClass('show-menu');
+	    if (menu.hasClass('show-menu')) {
+	      container.css('background-color', "#EFEFEF");
+	      menu.slideDown(300);
+	      button.attr('aria-expanded', 'true');
+	      setupOutsideClickListener(menu, button, container);
 	    } else {
-	      dropdownMenuSidebarVariant.slideUp(300, function () {
-	        dropdownSidebarVariant.css('background-color', "");
-	      });
-	      dropdownButtonSidebarVariant.attr('aria-expanded', 'false');
+	      menu.slideUp(300, () => container.css('background-color', ""));
+	      button.attr('aria-expanded', 'false');
 	    }
-	  });
-	  dropdownButtonHeaderVariant.on('click', function () {
-	    caretIcon.toggleClass('rotated');
-	    dropdownMenuHeaderVariant.toggleClass('show-menu');
-	    if (dropdownMenuHeaderVariant.hasClass('show-menu')) {
-	      dropdownHeaderVariant.css('background-color', "#EFEFEF");
-	      dropdownMenuHeaderVariant.slideDown(300);
-	      dropdownButtonHeaderVariant.attr('aria-expanded', 'true');
-	    } else {
-	      dropdownMenuHeaderVariant.slideUp(300, function () {
-	        dropdownHeaderVariant.css('background-color', "");
-	      });
-	      dropdownButtonHeaderVariant.attr('aria-expanded', 'false');
+	  }
+	  function setupOutsideClickListener(menu, button, container) {
+	    function outsideClickHandler(event) {
+	      if (!menu.is(event.target) && menu.has(event.target).length === 0 && !button.is(event.target) && button.has(event.target).length === 0) {
+	        menu.removeClass('show-menu');
+	        menu.slideUp(300, () => container.css('background-color', ""));
+	        button.attr('aria-expanded', 'false');
+	        container.find('.caret-icon').removeClass('rotated');
+	        $(document).off('click', outsideClickHandler);
+	      }
 	    }
-	  });
+	    setTimeout(() => {
+	      $(document).on('click', outsideClickHandler);
+	    }, 0);
+	  }
 
-	  //no need to repeat these functions with event delegation. 
-	  $('.widget-area .category-dropdown-menu .menu-item').on('click', function (e) {
-	    e.preventDefault();
-	    $('widget-area .caret-icon').toggleClass('rotated');
-
-	    // Make all items visible again before hiding the clicked one
-	    $('.widget-area .category-dropdown-menu .menu-item').removeClass('hidden').addClass('visible');
-
-	    // Hide the clicked item
-	    $(this).addClass('hidden').removeClass('visible');
-	    var categoryText = $(this).text();
-	    $('.widget-area #dropdown-btn-text').text(categoryText);
-	    if (categoryText === 'All work') {
-	      filterContentByCategoryAjax($, {
-	        category: 'All work'
-	      });
-	    }
-	    filterContentByCategoryAjax($, {
-	      category: categoryText
+	  // Event delegation for click on dropdown buttons
+	  $('body').on('click', '.category-dropdown__menu-btn', function () {
+	    const isSidebar = $(this).closest('.widget-area').length > 0;
+	    const context = isSidebar ? '.widget-area' : '.site-main';
+	    toggleDropdown({
+	      button: $(this),
+	      menu: $(`${context} .category-dropdown-menu`),
+	      container: $(`${context} .category-dropdown`)
 	    });
 	  });
-	  $('.site-main .category-dropdown-menu .menu-item').on('click', function (e) {
+
+	  // Event delegation for dropdown menu item clicks
+	  $('body').on('click', '.category-dropdown-menu .menu-item', function (e) {
 	    e.preventDefault();
-	    $('.site-main .caret-icon').toggleClass('rotated');
-
-	    // Make all items visible again before hiding the clicked one
-	    $('.site-main .category-dropdown-menu .menu-item').removeClass('hidden').addClass('visible');
-
-	    // Hide the clicked item
-	    $(this).addClass('hidden').removeClass('visible');
-	    var categoryText = $(this).text();
-	    $('.site-main #dropdown-btn-text').text(categoryText);
-	    if (categoryText === 'All work') {
-	      filterContentByCategoryAjax($, {
-	        category: "All work"
-	      });
-	    }
+	    const $this = $(this);
+	    const isSidebar = $this.closest('.widget-area').length > 0;
+	    const context = isSidebar ? '.widget-area' : '.site-main';
+	    $(`${context} .caret-icon`).toggleClass('rotated');
+	    $(`${context} .category-dropdown-menu .menu-item`).removeClass('hidden').addClass('visible');
+	    $this.addClass('hidden').removeClass('visible');
+	    const categoryText = $this.text();
+	    $(`${context} #dropdown-btn-text`).text(categoryText);
 	    filterContentByCategoryAjax($, {
 	      category: categoryText
 	    });
